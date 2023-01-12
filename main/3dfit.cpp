053bdb910761622ceefdea45c7c2b703580ef8fd
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-01-12 18:26:17 +0000

    WorkShop ROOT NFIST 12/jan/2022

--- main/3dfit.cpp
+++ main/3dfit.cpp
@@ -1,5 +1,4 @@
 #include "ReadData.h"
-
 #include "TGraph2D.h"
 #include "TStyle.h"
 #include "TCanvas.h"
@@ -8,11 +7,6 @@
 #include "TRootCanvas.h"
 #include "TApplication.h"
 
-double function(double *x, double *par)
-{
-    double result = par[0] * exp(-0.5 * pow(((x[0] - par[1]) / par[2]), 2)) - 0.5 * pow(((x[1] - par[3]) / par[4]), 2) + par[5] * exp(-0.5 * pow(((x[0] - par[6]) / par[7]), 2)) - 0.5 * pow(((x[1] - par[8]) / par[9]), 2);
-    return result;
-}
 
 int main()
 {
@@ -36,17 +30,23 @@ int main()
     TCanvas *c = new TCanvas("canvas", "grafico", 200, 10, 1920, 1080);
 
     TGraph2D *gr = new TGraph2D(data.size(), x, y, z);
+    
     TF2 *f = new TF2("func", "xygaus(0) + xygaus(5)", -2.5, 2, -2.5, 2);
     f->SetParameters(20, 1, 0.4, -0.8, 0.4, 10, 0.1, 0.3, 0.6, 0.2);
-    auto param = f->GetParameters();
+    f->SetTitle("Plot & Fit 3D;Eixo X;Eixo Y; Eixo Z");
+
 
     gr->Fit(f);
- 
     gr->SetMarkerStyle(20);
+ 
+ 
+
     c->Update();
+
+    
     
     f->Draw("surf1");
-    gr->Draw("same P");
+    gr->Draw("same P0");
     c->SaveAs("tomografia.png");
     return 0;
 }
